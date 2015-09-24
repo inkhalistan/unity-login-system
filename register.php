f7ccb15193bcead5ffb8798ef95a464d75218ffe
SaltPeter <peter.tran3@carleton.ca>
2015-09-24 08:33:36 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,7 @@
 <?php
+$link = mysql_connect("host", "username", "password");
+$db_selected = mysql_select_db("databas", $link);
+
 $username = $_REQUEST['username']; 
 $password = $_REQUEST['password']; 
 $email = $_REQUEST['email'];
@@ -7,6 +10,13 @@ $user = 'secondhand_tcg';
 $dbpassword = '111111'; 
 $db = 'secondhand_tcg'; 
 $table = 'users'; 
+
+function fetchinfo($rowname,$tablename,$finder,$findervalue) {
+	if($finder == "1") $result = mysql_query("SELECT $rowname FROM $tablename");
+	else $result = mysql_query("SELECT $rowname FROM $tablename WHERE `$finder`='$findervalue'");
+	$row = mysql_fetch_assoc($result);
+	return $row[$rowname];
+}
 
 mysql_connect($host, $user, $dbpassword) or die(mysql_error()); 
 mysql_select_db($db); 
@@ -22,11 +32,7 @@ if ($numrows == 0)
 else {
 	while($row = mysql_fetch_assoc($findid))
 		$userid = $row['id'] ;  break;
-
-$startingcollectioncards = array(0,0,1,1,2,3,3,4,5,6,0,1,2,3,4,5,6,7,8,9,10,10,11,11,11);
-	foreach ($startingcollectioncards as $card)
-		mysql_query("INSERT INTO `player_collections` VALUES ('NULL', '{$userid}', '{$card}')") or die (mysql_error()); 
-
+		
 	$startingdeckcards = array(0,1,2,3,4,5,6,7,8,9,10,0,1,2,3,4,5,6,7,8,9,10,11);
 	foreach ($startingdeckcards as $card)
 		mysql_query("INSERT INTO `player_decks` VALUES ('NULL', '{$userid}', '{$card}')") or die (mysql_error()); 
