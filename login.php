565b63ebb305e1f6269f96742cd826d7cbf35ad5
SaltPeter <peter.tran3@carleton.ca>
2015-09-24 08:20:54 -0400

    Update login.php

--- login.php
+++ login.php
@@ -12,14 +12,14 @@ mysql_select_db($db);
 $check = mysql_query("SELECT * FROM `users` WHERE `username`='".$username."'" ) or die (mysql_error());
 $numrows = mysql_num_rows($check);
 if ($numrows == 0)
-		die("Username doesn't exist \n");
+	die("Username doesn't exist \n");
 else {
 	$password = md5($password);
 	while ($row = mysql_fetch_assoc($check)) {//finds the rows that have our username
-	if($password == $row['password'])
-		die("login-SUCCESS".$row['id']);
-	else
-		die("Password doesn't match \n");
+		if($password == $row['password'])
+			die("login-SUCCESS".$row['id']);
+		else
+			die("Password doesn't match \n");
 	}
 }
 ?>
