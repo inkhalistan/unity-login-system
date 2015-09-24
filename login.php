b69abb583187fe9671cf8257311e618dbe0e9e9f
SaltPeter <peter.tran3@carleton.ca>
2015-09-24 10:03:34 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,18 @@
 <?php
+$link = mysql_connect("host", "username", "password");
+
 $username = $_REQUEST['username']; 
 $password = $_REQUEST['password']; 
 
-mysql_connect($host, $user, $dbpassword) or die(mysql_error()); 
-mysql_select_db($db); 
+mysql_connect($link) or die(mysql_error()); 
+$db_selected = mysql_select_db("databas", $link);
 
 $check = mysql_query("SELECT * FROM `users` WHERE `username`='".$username."'" ) or die (mysql_error());
 $numrows = mysql_num_rows($check);
 if ($numrows == 0)
 	die("Username doesn't exist \n");
 else {
-	$password = md5($password);
+	$password = password_hash($password, PASSWORD_BCRYPT);
 	while ($row = mysql_fetch_assoc($check)) {//finds the rows that have our username
 		if($password == $row['password'])
 			die("login-SUCCESS".$row['id']);
